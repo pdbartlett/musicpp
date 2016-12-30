499f01b968419cc3aec33134ab4720ff22b544db
Paul Bartlett <pdbartlett@gmail.com>
2016-12-30 13:39:25 +0000

    Add comments to interval table

--- src/musicpp.cc
+++ src/musicpp.cc
@@ -2,29 +2,30 @@
 
 static Interval INTERVAL_TABLE[12][2] = {
   { Interval(IntervalQuality::PERFECT,    IntervalNumber::UNISON),
-    Interval(IntervalQuality::DIMINISHED, IntervalNumber::SECOND)  },
+    Interval(IntervalQuality::DIMINISHED, IntervalNumber::SECOND)  },  // 0 semitones
   { Interval(IntervalQuality::MINOR,      IntervalNumber::SECOND),
-    Interval(IntervalQuality::AUGMENTED,  IntervalNumber::UNISON)  },
+    Interval(IntervalQuality::AUGMENTED,  IntervalNumber::UNISON)  },  // 1 semitones
   { Interval(IntervalQuality::MAJOR,      IntervalNumber::SECOND),
-    Interval(IntervalQuality::DIMINISHED, IntervalNumber::THIRD)   },
+    Interval(IntervalQuality::DIMINISHED, IntervalNumber::THIRD)   },  // 2 semitones
   { Interval(IntervalQuality::MINOR,      IntervalNumber::THIRD),
-    Interval(IntervalQuality::AUGMENTED,  IntervalNumber::SECOND)  },
+    Interval(IntervalQuality::AUGMENTED,  IntervalNumber::SECOND)  },  // 3 semitones
   { Interval(IntervalQuality::MAJOR,      IntervalNumber::THIRD),
-    Interval(IntervalQuality::DIMINISHED, IntervalNumber::FOURTH)  },
+    Interval(IntervalQuality::DIMINISHED, IntervalNumber::FOURTH)  },  // 4 semitones
   { Interval(IntervalQuality::PERFECT,    IntervalNumber::FOURTH),
-    Interval(IntervalQuality::AUGMENTED,  IntervalNumber::THIRD)   },
+    Interval(IntervalQuality::AUGMENTED,  IntervalNumber::THIRD)   },  // 5 semitones
   { Interval(IntervalQuality::AUGMENTED,  IntervalNumber::FOURTH),
-    Interval(IntervalQuality::DIMINISHED, IntervalNumber::FIFTH)   },
+    Interval(IntervalQuality::DIMINISHED, IntervalNumber::FIFTH)   },  // 6 semitones
   { Interval(IntervalQuality::PERFECT,    IntervalNumber::FIFTH),
-    Interval(IntervalQuality::DIMINISHED, IntervalNumber::SIXTH)   },
+    Interval(IntervalQuality::DIMINISHED, IntervalNumber::SIXTH)   },  // 7 semitones
   { Interval(IntervalQuality::MINOR,      IntervalNumber::SIXTH),
-    Interval(IntervalQuality::AUGMENTED,  IntervalNumber::FIFTH)   },
+    Interval(IntervalQuality::AUGMENTED,  IntervalNumber::FIFTH)   },  // 8 semitones
   { Interval(IntervalQuality::MAJOR,      IntervalNumber::SIXTH),
-    Interval(IntervalQuality::DIMINISHED, IntervalNumber::SEVENTH) },
+    Interval(IntervalQuality::DIMINISHED, IntervalNumber::SEVENTH) },  // 9 semitones
   { Interval(IntervalQuality::MINOR,      IntervalNumber::SEVENTH),
-    Interval(IntervalQuality::AUGMENTED,  IntervalNumber::SIXTH)   },
+    Interval(IntervalQuality::AUGMENTED,  IntervalNumber::SIXTH)   },  // 10 semitones
   { Interval(IntervalQuality::MAJOR,      IntervalNumber::SEVENTH),
-    Interval(IntervalQuality::DIMINISHED, IntervalNumber::UNISON)  }};
+    Interval(IntervalQuality::DIMINISHED, IntervalNumber::UNISON)  },  // 11 semitones
+};
 
 PitchClass NamedPitch::Class() const {
   return static_cast<PitchClass>((static_cast<int>(BASE_CLASS_PITCHES[static_cast<int>(p_)]) +
